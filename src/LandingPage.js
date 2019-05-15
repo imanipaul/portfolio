3c4a51396dc7fc073bb6db10bdbbee2a474456f0
imanipaul <imanipaul@wustl.edu>
2019-05-15 15:34:43 -0400

    Add header and nav

---
+++ src/LandingPage.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function LandingPage(props) {
+    return (
+        <div className='landing-page-container'>
+            <header>
+                <div>github image</div>
+                <h1>Imani Paul</h1>
+                <div>linkedin image</div>
+            </header>
+            <nav>
+                <div>About</div>
+                <div>Projects</div>
+                <div>Contact</div>
+            </nav>
+
+
+        </div>
+    )
+}
+
+export default LandingPage
