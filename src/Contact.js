92de537cc89affaca1c29bf7d55b9f069eff8012
imanipaul <imanipaul@wustl.edu>
2019-05-15 19:37:52 -0400

    Add logo images

--- src/Contact.js
+++ src/Contact.js
@@ -3,11 +3,16 @@ import React from 'react'
 function Contact(props) {
     return (
         <div className='contact-container'>
-            <form>
-                <input name='name' type='text' placeholder='Name' />
-                <input name='email' type='text' placeholder='Email' />
-                <input name='message' type='text' placeholder='Message' />
-            </form>
+            <div className='about-nav'>
+                <h2>Imani Paul</h2>
+                <p>About</p>
+                <p>Projects</p>
+                <p>Contact</p>
+            </div>
+            <h3>Contact me!</h3>
+            <a href='mailto:imani.paul1@gmail.com'>Email me!</a>
+            <a href='http://www.linkedin.com/in/imani-paul' target='_blank'>LinkedIn</a>
+            <a href='https://github.com/imanipaul' target='_blank'>Github</a>
         </div>
     )
 }
