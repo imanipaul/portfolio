989bbbeefc718a8a775399e6e46c6d0e9c4de9a5
imanipaul <imanipaul@wustl.edu>
2019-05-15 17:00:14 -0400

    Style project page

--- src/Projects.js
+++ src/Projects.js
@@ -3,20 +3,54 @@ import React from 'react'
 function Projects(props) {
     return (
         <div className='projects-container'>
-            <div>
-                Project 1
+            <div className='about-nav'>
+                <h2>Imani Paul</h2>
+                <p>About</p>
+                <p>Projects</p>
+                <p>Contact</p>
             </div>
-            <div>
-                Project 2
-            </div>
-            <div>
-                Project 3
-            </div>
-            <div>
-                Project 4
-            </div>
-            <div>
-                Project 5
+            <div className='all-projects'>
+
+
+                <div className='project-card'>
+                    Project 1
+                    <div className='project-card-description'>
+                        Project 1 Description
+                </div>
+                </div>
+
+
+                <div className='project-card'>
+                    Project 2
+                    <div className='project-card-description'>
+                        Project 2 Description
+                </div>
+                </div>
+
+
+                <div className='project-card'>
+                    Project 3
+                    <div className='project-card-description'>
+                        Project 3 Description
+                </div>
+                </div>
+
+
+                <div className='project-card'>
+                    Project 4
+                    <div className='project-card-description'>
+                        Project 4 Description
+                </div>
+                </div>
+
+
+                <div className='project-card'>
+                    Project 5
+                    <div className='project-card-description'>
+                        Project 5 Description
+                </div>
+                </div>
+
             </div>
         </div>
     )
