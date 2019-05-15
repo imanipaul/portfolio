01b8965515419ac26e5a55e744411f5396c0ca8e
imanipaul <imanipaul@wustl.edu>
2019-05-15 15:37:41 -0400

    Download icons and create About component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import LandingPage from './LandingPage'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-   
+      <LandingPage />
+
     </div>
   );
 }
