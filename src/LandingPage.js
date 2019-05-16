544cdb2c60f7378e4de645557417ff3046c04c3f
imanipaul <imanipaul@wustl.edu>
2019-05-15 23:50:53 -0400

    Add background image to landing page

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import github from './images/github.png'
 import linkedin from './images/linkedin.png'
+import keyboard from './images/keyboard.jpg'
+import computercode from './images/computercode.jpg'
+import computercoffee from './images/computercoffee.jpg'
+import computerglasses from './images/computerglasses.jpg'
+import computerplant from './images/computerplant.jpg'
+import closedcomputer from './images/closedcomputer.jpg'
+import computercup from './images/computercup.jpg'
 
-function LandingPage(props) {
+function LandingPage() {
     return (
         <div className='landing-page-container'>
             <header>
@@ -20,16 +27,9 @@ function LandingPage(props) {
                 <div>Contact</div>
             </nav>
             <section>
-                Content
+                <img src={computercup} />
+                <div className='web-developer'>Web Developer</div>
             </section>
-            <section>
-                Content
-            </section>
-            <section>
-                Content
-            </section>
-
-
         </div>
     )
 }
