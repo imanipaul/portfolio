ec0e460ec90fb296f43c3e9183c7a3d5dcb8ddf2
imanipaul <imanipaul@wustl.edu>
2019-05-15 22:13:52 -0400

    Style project image overlays

--- src/Projects.js
+++ src/Projects.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import speedtype from './images/speedtype.png'
+import recollect from './images/recollect.png'
+import chumleys from './images/chumleys.png'
+import ensemble from './images/ensemble.png'
+import allthingsmusic from './images/allthingsmusic.png'
 
 function Projects(props) {
     return (
@@ -13,45 +18,62 @@ function Projects(props) {
 
 
                 <div className='project-card'>
-                    Project 1
-                    <div className='project-card-description'>
-                        Project 1 Description
-                </div>
-                </div>
-
-
-                <div className='project-card'>
-                    Project 2
-                    <div className='project-card-description'>
-                        Project 2 Description
-                </div>
+                    <img className='project-card-image' alt='speedtype' src={speedtype} />
+                    <div className='hover-text'>
+                        <div className='text-description'>
+                            <h3>Speed Type</h3>
+                            <p>Typing game using vanilla Javascript and CSS animations</p>
+                        </div>
+                    </div>
                 </div>
 
 
                 <div className='project-card'>
-                    Project 3
-                    <div className='project-card-description'>
-                        Project 3 Description
-                </div>
+                    <img className='project-card-image' alt='All Things Music' src={allthingsmusic} />
+                    <div className='hover-text'>
+                        <div className='text-description'>
+                            <h3>All Thing Music</h3>
+                            <p>Popular music search engine using React and LastFM API</p>
+                        </div>
+                    </div>
+
                 </div>
 
 
                 <div className='project-card'>
-                    Project 4
-                    <div className='project-card-description'>
-                        Project 4 Description
-                </div>
+                    <img className='project-card-image' alt='Ensemble' src={ensemble} />
+                    <div className='hover-text'>
+                        <div className='text-description'>
+                            <h3>Ensemble</h3>
+                            <p>A full stack site for connecting with fellow students and instructors</p>
+                        </div>
+                    </div>
                 </div>
 
 
                 <div className='project-card'>
-                    Project 5
-                    <div className='project-card-description'>
-                        Project 5 Description
-                </div>
+                    <img className='project-card-image' alt='chumleys' src={chumleys} />
+                    <div className='hover-text'>
+                        <div className='text-description'>
+                            <h3>Chumley's</h3>
+                            <p>Restaurant site redesign for local NYC eatery.
+                            Created through collaboration with UX designers</p>
+                        </div>
+                    </div>
                 </div>
 
+
+                <div className='project-card' >
+                    <img className='project-card-image' alt='ReCollect' src={recollect} />
+                    <div className='hover-text'>
+                        <div className='text-description'>
+                            <h3>ReCollect</h3>
+                            <p>Full stack inventory app for household essentials</p>
+                        </div>
+                    </div>
+                </div>
             </div>
+
         </div>
     )
 }
