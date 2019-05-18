f320bfacc46f414fef3e71cdff31fa6f8218ddc8
imanipaul <imanipaul@wustl.edu>
2019-05-18 14:30:59 -0400

    Update background image

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import github from './images/github.png'
-import linkedin from './images/linkedin.png'
 
 
 function LandingPage() {
@@ -8,11 +6,9 @@ function LandingPage() {
         <div className='landing-page-container'>
             <header>
                 <div className='logos'>
-                    {/* <a href='https://github.com/imanipaul' target='_blank'><img alt='github logo' src={github} /></a> */}
                 </div>
                 <h1>Imani Paul</h1>
                 <div className='logos'>
-                    {/* <a href='https://www.linkedin.com/in/imani-paul/' target='_blank'><img alt='linkedin logo' src={linkedin} /></a> */}
                 </div>
             </header>
             <nav>
